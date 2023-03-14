2bc5d4faec4f197ecef0ecd9088515185e72b547
sanjay-2611 <sanjay261101@gmail.com>
2023-03-14 20:41:37 +0530

    update

--- src/test/java/StepDefs/AddItemToCart.java
+++ src/test/java/StepDefs/AddItemToCart.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -24,8 +26,9 @@ public class AddItemToCart {
 	@BeforeAll
 	public static void startup() {
 				
-		WebDriverManager.chromedriver().setup();
-	    driver=new ChromeDriver();
+		WebDriverManager.chromedriver().driverVersion("111.0.5563.65").setup();
+		
+		driver = new ChromeDriver();
 	    driver.manage().window().maximize();
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 	    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
@@ -45,32 +48,29 @@ public class AddItemToCart {
 	}
 	
 	
-	@When("Add Item to cart")
-	public void add_item_to_cart(DataTable dataTable) {
-		wait= new WebDriverWait(driver, Duration.ofSeconds(30));
-		List<String> data= dataTable.asList();
-		String item= data.get(1);
-		item1=item;
+	@When("Add Item to cart {string}")
+	public void add_item_to_cart(String string)  {
+		
 		
 		WebElement home= driver.findElement(By.xpath("//li/a[contains(text(),'Home')]"));
 		  wait.until(ExpectedConditions.elementToBeClickable(home));
 		  home.click();
-		  driver.findElement(By.linkText(item)).click();
+		  driver.findElement(By.linkText(string)).click();
 		  driver.findElement(By.xpath("//a[contains(text(),'Add to cart')]")).click();
 		  wait.until(ExpectedConditions.alertIsPresent());
 		  Alert alert=driver.switchTo().alert();		  
 		  alert.accept();
 	    
 	}
-	@Then("Item must added to the cart")
-	public void item_must_added_to_the_cart() {
+	@Then("Item must added to the cart {string}")
+	public void item_must_added_to_the_cart(String string1) {
 		boolean count =false;
 		driver.findElement(By.xpath("//a[contains(text(),'Cart')]")).click();
 		  List<WebElement> cartlist=driver.findElements(By.xpath("//td[2]"));
 		  for(WebElement cartslist: cartlist)
 		  {
-			  if(cartslist.getText().equalsIgnoreCase(item1)) {
-			  Assert.assertEquals(cartslist.getText(), item1);
+			  if(cartslist.getText().equalsIgnoreCase(string1)) {
+			  Assert.assertEquals(cartslist.getText(), string1);
 			  count=true;
 			  }		  
 		  }
--- src/test/resources/features/demoblaze.feature
+++ src/test/resources/features/demoblaze.feature
@@ -2,19 +2,21 @@
 Feature: Automating demoblaze website
 
   
-  Scenario: Add Item to cart
+  Scenario Outline: Add Item to cart
   
     Given Login Into App
     
-    When Add Item to cart
-    	|item|
+    When Add Item to cart "<item>"
+    Then Item must added to the cart "<item>"
+    
+	Examples:
+		|item|
 			|Samsung galaxy s7| 
 			|Nokia lumia 1520|
-#			|Iphone 6 32gb|
-#			|Sony vaio i7| 
-    Then Item must added to the cart
-    
-
+			|Iphone 6 32gb|
+			|Sony vaio i7|
+			
+			
 	Scenario: Delete an Item
 #		Given Login Into App
 		When  List of items available in the cart
